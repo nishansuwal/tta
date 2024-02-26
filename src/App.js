bfec6546e80684066dff8da713351e2159406ecf
nishan <nishansuwal2020@gmail.com>
2024-02-26 10:57:05 +0545

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SiteLayout from "./components/site/siteLayout"
+
+import Dashboard from './pages/client/dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <SiteLayout>
+              <Dashboard />
+            </SiteLayout>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/client/dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className='bg-blue-600'>Dashboard</div>
+  )
+}
+
+export default Dashboard
